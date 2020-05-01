e030f06008ffca2c5f0166a1b206c6628fda5437
Julio Suriano <julios@vt.edu>
2020-04-30 18:09:13 -0600

    solved for rows and columns

--- nqueens.py
+++ nqueens.py
@@ -1,14 +1,14 @@
 import numpy as np
 
-N = 4
+X = 4
 
-starting_grid = np.zeros((N, N))
+starting_grid = np.zeros((X, X))
 
 # Function to understand the idea of rows, columns
 # and diagonals in a matrix
 # NOTE: this function is not used in the solution
 def queenAttackingDirections(grid, r, c):
-    for i in range(N):
+    for i in range(X):
         # ROW ----------
         grid[r][i] = 1
         
@@ -23,13 +23,31 @@ def queenAttackingDirections(grid, r, c):
         # apply the same distance to the column (ignore when out of bounds)
         # +j and -j for diagonals in both sides
 
-        if c+j < N:
+        if c+j < X:
             grid[i][c+j] = 1
         
         if c-j >= 0:
             grid[i][c-j] = 1
 
     return grid
-print(queenAttackingDirections(starting_grid, N//2, N//2))
+#print(queenAttackingDirections(starting_grid, X//2, X//2))
 
 # N QUEENS SOLUTION
+def isValid(N, grid, r, c):
+    for i in range(N):
+        if grid[i][c] == 1:
+            return False
+    return True
+
+def placeQueen(N, grid, r):
+    for i in range(N):
+        if isValid(N, grid, r, i):
+            grid[r][i] = 1
+            placeQueen(N, grid, r+1)
+
+def solve(N):
+    grid = np.zeros((N,N))
+    placeQueen(N, grid, 0)
+    return grid
+
+print(solve(4))
