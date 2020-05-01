afa0709b9e555a1d23da9c51ddb316aa784cb5ab
Julio Suriano <julios@vt.edu>
2020-05-01 11:12:25 -0600

    Updated

--- roman_to_integer.py
+++ roman_to_integer.py
@@ -69,3 +69,29 @@ for i, d in enumerate(roman):
     
 print(num)
 print()
+
+# METHOD 2 - - - - -
+print('METHOD 2')
+
+# LOGIC: 
+# A solution that refers to a dictionary with valid roman numerals and shortens
+# the original string
+
+ROMAN_TO_DEC = {
+    'I': 1, 'IV': 4, 'V': 5, 'IX': 9, 
+    'X': 10, 'XL': 40, 'L':50, 'XC':90, 
+    'C':100, 'CD': 400, 'D': 500, 'CM': 900, 
+    'M': 1000,
+}
+
+dec = 0
+
+while roman != '':
+    if roman[:2] in ROMAN_TO_DEC:
+        dec += ROMAN_TO_DEC[roman[:2]]
+        roman = roman[2:]
+    else:
+        dec += ROMAN_TO_DEC[roman[:1]]
+        roman = roman[1:]
+
+print(dec)
