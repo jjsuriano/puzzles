4b49dacce8fdfd8a24de3181406664686965ba8f
Julio Suriano <julios@vt.edu>
2020-05-04 18:29:15 -0600

    Added second method

--- reverse_string.py
+++ reverse_string.py
@@ -1,4 +1,3 @@
-
 # DESCRIPTION:
 # Create your own reverse function that reverses the characters in a string
 # Cannot use string[::-1] or reversed(string)
@@ -29,4 +28,21 @@ def reverse_string(s):
     return reverse_string(s[1:]) + s[0]
 
 print('The reversed version of the string is: \'' + reverse_string(string) + '\'.')
+print()
+
+# METHOD 2 - - - - -
+print('METHOD 2')
+# LOGIC: 
+# Using the idea of two pointers and swapping them until they meet in the middle
+reversed_str = list(string)
+i = 0
+j = len(reversed_str)-1
+while (i < j):
+    reversed_str[i], reversed_str[j] = reversed_str[j], reversed_str[i]
+    i += 1
+    j -= 1
+
+reversed_str = ''.join(reversed_str)
+
+print('The reversed version of the string is: \'' + reversed_str + '\'.')
 print()
