77bf438c509b1576aab4625ae665c3c7f9c74299
Julio Suriano <julios@vt.edu>
2020-05-05 09:24:28 -0600

    Added Method 3 and updated Method 2

--- reverse_string.py
+++ reverse_string.py
@@ -34,15 +34,39 @@ print()
 print('METHOD 2')
 # LOGIC: 
 # Using the idea of two pointers and swapping them until they meet in the middle
-reversed_str = list(string)
+# This method uses a list to swap the charaters
+
+list_str = list(string)
 i = 0
-j = len(reversed_str)-1
+j = len(list_str) - 1
 while (i < j):
-    reversed_str[i], reversed_str[j] = reversed_str[j], reversed_str[i]
+    if list_str[i] != list_str[j]:
+        list_str[i], list_str[j] = list_str[j], list_str[i]
     i += 1
     j -= 1
 
-reversed_str = ''.join(reversed_str)
+reversed_str = ''.join(list_str)
 
 print('The reversed version of the string is: \'' + reversed_str + '\'.')
+print()
+
+# METHOD 3 - - - - -
+print('METHOD 3')
+# LOGIC: 
+# Using the idea of two pointers and swapping them until they meet in the middle
+# This method uses string slicing and concatenation to swap characters
+
+i = 0
+j = len(string) - 1
+copy = string
+
+while i < j:
+    prefix = copy[:i]
+    middle = copy[i+1:j]
+    suffix = copy[j+1:]
+    copy = prefix + copy[j] + middle + copy[i] + suffix
+    i += 1
+    j -= 1
+
+print('The reversed version of the string is: \'' + copy + '\'.')
 print()
