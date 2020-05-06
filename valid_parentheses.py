fdf14a4bc03bfeeeeddc3e390f58f7a82df1bdf9
Julio Suriano <julios@vt.edu>
2020-05-06 14:29:09 -0600

    Valid parentheses

---
+++ valid_parentheses.py
@@ -0,0 +1,41 @@
+# DESCRIPTION:
+# Given a string with different parentheses characters, determine if it is 
+# valid.
+
+# EXAMPLE:
+# "()[]{}" -> True
+
+# INPUT: 
+# A string with a set of parentheses
+
+# OUTPUT: 
+# A boolean that states if it is a valid pattern of parentheses
+
+input_str = "}"
+print()
+
+# METHOD 1 - - - - -
+print('METHOD 1')
+
+# LOGIC: 
+# This method uses a stack to keep track of the opening character and match the
+# last in the stack with a closing character
+
+def valid(input_str):
+    stack = []
+    pairs = {'}': '{', ']': '[', ')': '('}
+
+    for i in input_str:
+        if i in pairs.values():
+            stack.append(i)
+        if i in pairs.keys():
+            if len(stack) == 0 or stack.pop() != pairs[i]:
+                return False
+
+    if len(stack) != 0:
+        return False
+
+    return True
+
+print(valid(input_str))
+print()
