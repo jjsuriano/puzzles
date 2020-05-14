6b58e82a729b27e0edb8f11740221bbf6bf3bc76
Julio Suriano <julios@vt.edu>
2020-05-14 09:50:49 -0600

    Reorganized folder structure to add Go

---
+++ Python/trapping_water.py
@@ -0,0 +1,23 @@
+
+# DESCRIPTION:
+# Given n non-negative integers representing an elevation map where the width
+# of each bar is 1, compute how much water it is able to trap after raining.
+
+# EXAMPLE:
+# [0,1,0,2,1,0,1,3,2,1,2,1] -> 6
+
+# INPUT: 
+# An array of the elevations
+
+# OUTPUT: 
+# A integer of the area that traps water
+
+elevation = [0,1,0,2,1,0,1,3,2,1,2,1]
+
+print()
+
+# METHOD 1 - - - - -
+print('METHOD 1')
+
+# LOGIC: 
+# 
