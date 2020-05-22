7d779fd8c5fe6c5e459c425c5ffea81d5618f133
Julio Suriano <julios@vt.edu>
2020-05-22 08:50:30 -0600

    Fixed bug when num=2

--- Python/isPrime.py
+++ Python/isPrime.py
@@ -14,8 +14,6 @@
 # OUTPUT: 
 # A bool, true if the input is a prime number, false if it is a composite number
 
-num = 12
-
 print()
 
 # METHOD 1 - - - - -
@@ -26,6 +24,7 @@ print('METHOD 1')
 # than num and check if num is divisible by any of those.
 
 # To improve the efficiency of the function I:
+# - return true if it is 2 (the only prime even number)
 # - returned false immediately if num is even or 1
 # - looped only through odd numbers
 # - looped until num//i is greater than 1 because we are never going to reach 1 in
@@ -33,7 +32,11 @@ print('METHOD 1')
 #   the odd number before num. Once the quotient reaches 1, there is no point in 
 #   running the loop.
 
-def isPrime(x):
+def isPrime(x:int) -> bool:
+
+    if x == 2: 
+        return True
+
     if x%2 == 0 or x == 1:
         return False
     
@@ -45,10 +48,13 @@ def isPrime(x):
     
     return True
 
-if isPrime(num):
-    print('{} is a prime number.'.format(num))
-else: 
-    print('{} is NOT a prime number.'.format(num))
-
 print()
 
+# print from 1 to 30 and determine if they are prime or not
+for num in range(1, 31):
+    if isPrime(num):
+        print('{} is a prime number.'.format(num))
+    else: 
+        print('{} is NOT a prime number.'.format(num))
+
+print()
