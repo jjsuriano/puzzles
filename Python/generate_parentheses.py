1877b5c4c2d688308ca4508076a254e9c079fc76
Julio Suriano <julios@vt.edu>
2020-06-26 13:36:47 -0600

    Update

--- Python/generate_parentheses.py
+++ Python/generate_parentheses.py
@@ -24,25 +24,28 @@ print('METHOD 1')
 # LOGIC: 
 # This method uses recursion to find all the possible combinations
 
-def matching_parentheses(opening, closing, current, combinations=None):
+def matching_parentheses(n):
+    return _matching_parentheses(n, n, "")
+
+def _matching_parentheses(opening, closing, current, combinations=None):
     if combinations is None:
         combinations = []
 
     if opening == 0 and closing == 0:
         combinations.append(current)
-        return
+        return combinations
 
     if opening == 0 or closing == 0 or (closing < opening):
-        return
+        return combinations
     
     opening -= 1
     current += '('
-    matching_parentheses(opening, closing, current, combinations)
+    _matching_parentheses(opening, closing, current, combinations)
     while closing > 0:
         closing -= 1
         current += ')'
-        matching_parentheses(opening, closing, current, combinations)
+        _matching_parentheses(opening, closing, current, combinations)
     return combinations
 
-print(matching_parentheses(n, n, ''))
+print(matching_parentheses(3))
 print()
