50552c98a1082428b059f064a5b913bc941a8ad0
Julio Suriano <julios@vt.edu>
2020-06-27 12:05:30 -0600

    Added another way of doing it

--- Python/three_sum.py
+++ Python/three_sum.py
@@ -66,3 +66,46 @@ print()
 print("Input:", nums2)
 print(threeSum(nums2))
 print()
+
+# METHOD 2 - - - - -
+print('METHOD 2')
+
+# LOGIC: Loop through the list and using the current num and using the
+# twoSum technique in the rest of the list (dictionary)
+# this uses MORE memory than METHOD 1
+
+def threeSum(nums):
+    size = len(nums)
+    results = []
+
+    if size < 3:
+        return results
+
+    nums = sorted(nums)
+
+    for i in range(size - 2):
+        if not i or nums[i] != nums[i-1]:
+            difference = dict()
+            target = 0 - nums[i]
+            j = i + 1
+            while j < size:
+                if nums[j] in difference:
+                    results.append([nums[i], difference[nums[j]], nums[j]])
+                    while j < size and nums[j-1] == nums[j]:
+                        j += 1
+                else:
+                    difference[target - nums[j]] = nums[j]
+                j += 1
+    return results
+
+print("Input:", nums)
+print(threeSum(nums))
+print()
+
+print("Input:", nums1)
+print(threeSum(nums1))
+print()
+
+print("Input:", nums2)
+print(threeSum(nums2))
+print()
